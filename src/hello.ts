da661d9b67faf15d8e853c174cfda0fa8cc48d31
Freewind <nowindlee@gmail.com>
2020-05-05 22:39:06 +0800

    improve

--- src/hello.ts
+++ src/hello.ts
@@ -1,44 +1,38 @@
 import {graphql, GraphQLObjectType, GraphQLSchema} from 'graphql';
 import 'reflect-metadata';
-import {buildSchema} from 'type-graphql';
+import {buildSchemaSync} from 'type-graphql';
 import Book from './Book';
 import DummyResolver from './dummy/DummyResolver';
 
-async function main() {
-  const schemaFromTypeGraphql = await buildSchema({
-    resolvers: [DummyResolver],
-    validate: false, // https://github.com/MichalLytek/type-graphql/issues/150
-    orphanedTypes: [Book],
-  });
+const schemaFromTypeGraphql = buildSchemaSync({
+  resolvers: [DummyResolver],
+  validate: false, // https://github.com/MichalLytek/type-graphql/issues/150
+  orphanedTypes: [Book],
+});
 
-  console.log(schemaFromTypeGraphql.getTypeMap())
+console.log(schemaFromTypeGraphql.getTypeMap())
 
-  const bookType = schemaFromTypeGraphql.getType('Book') as GraphQLObjectType;
+const bookType = schemaFromTypeGraphql.getType('Book') as GraphQLObjectType;
 
-  const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-      name: 'RootQueryType',
-      fields: {
-        book: {
-          type: bookType,
-          resolve() {
-            const book = new Book();
-            book.id = '111';
-            book.title = 'title111';
-            return book;
-          },
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      book: {
+        type: bookType,
+        resolve() {
+          const book = new Book();
+          book.id = '111';
+          book.title = 'title111';
+          return book;
         },
       },
-    }),
-  });
+    },
+  }),
+});
 
-  const query = '{ book {id, title} }';
+const query = '{ book {id, title} }';
 
-  graphql(schema, query).then(result => {
-    console.log(JSON.stringify(result));
-  });
-
-}
-
-main();
-
+graphql(schema, query).then(result => {
+  console.log(JSON.stringify(result));
+});
